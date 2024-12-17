cf1be46eb3453ba0216add2ce1d1898dc947ff36
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2024-12-17 11:59:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{ts,js,tsx}", // Adjust to match your project structure
+    "./public/index.html",             // Include public files if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+
