cd6921e82493360ea981baf403050df441cf64ef
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2024-12-30 14:03:00 +0530

    3rd 2nd part

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import mainbg from "../Images/BG1.jpg";
 import { CiSearch } from "react-icons/ci";
-import { WiCloud } from "react-icons/wi";
+import { WiCloud, WiHumidity } from "react-icons/wi";
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { FiSunrise, FiSunset } from "react-icons/fi";
+import { RiWindyFill } from "react-icons/ri";
+import { FaTemperatureHigh } from "react-icons/fa";
 
 const Home = () => {
   const api = {
     key: "59f86479b48512df06192c8043bba80e",
     base: "https://api.openweathermap.org/data/2.5/",
   };
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("Puttaparthi");
   const [weather, setWeather] = useState({});
+
   const searchPressed = () => {
     fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
@@ -18,17 +23,25 @@ const Home = () => {
         console.log(weather);
       });
   };
-  useEffect(() => {
-    setSearch("New Delhi");
-    setTimeout(() => searchPressed(), 0); // Ensure searchPressed runs after state update
-  }, []);
 
-    const sunriseTimestamp = weather?.sys?.sunrise;
-    const sunsetTimestamp = weather?.sys?.sunset;
-    const convertToTime = (timestamp) => {
-      const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
-      return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    }
+  // useEffect(() => {
+  //   setSearch("Puttaparthi");
+  //   setTimeout(() => searchPressed(), 0);
+  // }, []);
+
+  const sunriseTimestamp = weather?.sys?.sunrise;
+  const sunsetTimestamp = weather?.sys?.sunset;
+  const convertToTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  };
+
+  const renderCloudDescription = (clouds) => {
+    if (clouds <= 10) return "Clear Sky";
+    if (clouds <= 50) return "Partly Cloudy";
+    if (clouds <= 90) return "Mostly Cloudy";
+    return "Overcast";
+  };
   return (
     <div className="relative h-[100vh] w-[100%]">
       <img
@@ -59,28 +72,48 @@ const Home = () => {
           </p>
         </div>
         <div className="flex items-center justify-end">
-          <WiCloud className="text-white text-[170px] " />
+          <WiCloud className="text-white text-[170px]" />
           <div className="text-white font-semibold">
-            <p className="text-5xl">
-              5 o
+            <p className="text-[45px] flex items-center">
+              {Math.ceil(weather?.main?.temp)}
+              <TbTemperatureCelsius className="text-[40px]" />
             </p>
 
             {/* <p className="text-2xl">{weather.weather.id}</p> */}
           </div>
         </div>
+        <h1 className="absolute bottom-[6px] right-[120px] text-[25px] text-white font-bold">
+          {renderCloudDescription(weather?.clouds?.all)}
+        </h1>
       </div>
-      <div className="shadow-2xl h-[300px] w-[45%] absolute right-[20px] bg-transparent/5 top-[21%] rounded-lg flex flex-col">
-      <div>
-      <p>{weather?.main?.temp}</p>
-     <p>Wind<span className="">{weather?.wind?.speed}
-      </span> </p> 
-      <p>{weather?.main?.sea_level}</p>
-      <p>{convertToTime(sunriseTimestamp)}</p>
-      <p>{convertToTime(sunsetTimestamp)}</p>
-      </div>
-      <div>
-        
-      </div>
+      <div className="shadow-2xl h-[280px] w-[45%] absolute right-[20px] bg-transparent/5 top-[21%] rounded-lg flex flex-col p-[20px] justify-around ">
+        <div className="flex items-center justify-around">
+          <p>
+            <span className="flex items-center"><WiHumidity /> Humidity 
+              </span>{weather?.main?.humidity}
+          </p>
+          <p className="flex flex-col">
+            <span className="flex items-center">
+              Wind <RiWindyFill />
+            </span>
+            {weather?.wind?.speed}
+          </p>
+          
+          <p>
+            <FiSunrise />
+            {convertToTime(sunriseTimestamp)}
+          </p>
+        </div>
+        <div className="flex items-center justify-around">
+          <p className="flex flex-col"><span className="flex items-center"><FaTemperatureHigh />Feels Like
+          </span>{weather?.main?.feels_like}</p>
+          <p className="flex flex-col"><span>Sea Level
+            </span>{weather?.main?.sea_level}</p>
+          <p>
+            <FiSunset />
+            {convertToTime(sunsetTimestamp)}
+          </p>
+        </div>
       </div>
       <div className="flex flex-col absolute top-[64%] left-[7%] gap-[10px]">
         <h1 className="text-white text-3xl">ForeCast</h1>
